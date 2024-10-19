bc29bb0ebebc8f7f7246c8d675f060c5899a868e
hb0730 <1278032416@qq.com>
2024-10-19 12:19:59 +0800

    feat: p1 project

---
+++ zoom-p1-job/src/main/java/com/hb0730/zoom/ZoomP1JobApplication.java
@@ -0,0 +1,15 @@
+package com.hb0730.zoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author <a href="mailto:huangbing0730@gmail">hb0730</a>
+ * @date 2024/10/19
+ */
+@SpringBootApplication
+public class ZoomP1JobApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZoomP1JobApplication.class, args);
+    }
+}
---
+++ zoom-p1-job/src/main/java/com/hb0730/zoom/biz/job/SendMessageJob.java
@@ -0,0 +1,26 @@
+package com.hb0730.zoom.biz.job;
+
+import com.hb0730.zoom.base.service.proxy.SysProxyService;
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @author <a href="mailto:huangbing0730@gmail">hb0730</a>
+ * @date 2024/10/19
+ */
+@Slf4j
+@DisallowConcurrentExecution
+public class SendMessageJob implements Job {
+    @Autowired
+    private SysProxyService messageService;
+
+    @Override
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+        log.info("发送消息任务 SendMessageJob !");
+        messageService.doSendMessages(null);
+    }
+}
